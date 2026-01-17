eb101c7949be6e128863e5eb32f771f00fc67850
PhantomLunet <Teasir367@gmail.com>
2026-01-17 13:10:05 +0100

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ReverseAudioGame } from "@/components/reverse-audio-game"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-background">
-      <ReverseAudioGame />
-    </main>
-  )
-}
